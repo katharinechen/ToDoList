dd06389116400b508b2a3d2ef5332c528edef12a
Kyle Boe & Katharine Chen <student@epicodus.com>
2014-07-14 10:23:48 -0700

    second test commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var addToDo = function(name,task){
 
   // var masterList = [];
   var person = {"name": name, "task": task};
-
+  console.log(person);
   masterList.push(person);
 
   return masterList;
