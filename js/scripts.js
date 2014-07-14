54a59ce0d4ba889e3de04f643f20a92cf7c00298
Kyle Boe & Katharine Chen <student@epicodus.com>
2014-07-14 14:34:09 -0700

    Completed version

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
-var masterList = [];
+$(document).ready(function() {
 
-var addToDo = function(name,task){
+  $("#form").submit(function(event) {
 
-  // var masterList = [];
-  var person = {"name": name, "task": task};
-  console.log(person);
-  masterList.push(person);
+    var name = $("#name").val();
+    var priority = parseInt($("#priority").val());
+    var task = $("#task").val();
 
-  return masterList;
-};
+    var newTask = {"name": name, "priority": priority, "task": task };
 
-var deleteToDo = function (){
+    event.preventDefault();
 
-}
-// var masterList = function(objectValue) {
+    $("#results").show();
+    $("#results").append("<li class='entry'><span class='specific-task'>" + newTask.task + "</span></li>");
 
-// };
+    $("#name").val("");
+    $("#priority").val("");
+    $("#task").val("");
+
+    $(".specific-task").last().click(function() {
+      $("#full-list").show();
+      $("#full-list h2").text(newTask.name);
+      $('#name2').text(newTask.name);
+      $('div#priority2').html("<b>Priority: </b>" + newTask.priority);
+      $('div#task2').html("<b>Task Assignment: </b>" + newTask.task);
+      $('div#buttons').html("<button type='click' id='delete-button'>Mark as Done</button><button type='click' id='hide'>Hide</button>");
+
+      $("#delete-button").click(function(event) {
+        $(".specific-task").last().remove();
+        $("#full-list").hide();
+      });
+
+      $("#hide").last().click(function(event){
+        $("#full-list").hide();
+      });
+    });
+  });
+});
--- js/specs.js
+++
@@ -1,5 +0,0 @@
-describe("toDo", function(){
-  it("assigns a task to a person", function(){
-    toDo("Kyle","shopping").should.eql({name: "Kyle", task: "shopping"});
-  });
-});
